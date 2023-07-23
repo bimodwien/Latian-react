9ce1e1dba3124e0eeb88bdee2e723e09d3eb7ea0
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-07-23 19:30:16 +0700

    latian menggunakan useState dan useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Calculator from "./components/pr/Calculator";
-
+import Input from "./components/testing/Input";
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <br />
+      <Input />
     </div>
   );
 }
